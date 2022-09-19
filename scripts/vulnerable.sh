299cdcc2dcf3b2856902ecd87305c0e175943999
Spencer Heywood <l.spencer.heywood@protonmail.com>
2022-09-19 15:47:50 -0600

    updated vulnerable.sh to be cross-platform

--- scripts/vulnerable.sh
+++ scripts/vulnerable.sh
@@ -1,28 +1,23 @@
 #!/usr/bin/env bash
 
-pacman -Sy --noconfirm docker sudo
+# If Arch Linux
+if grep -q 'Arch Linux' /etc/os-release
+then
+        pacman -Syu --noconfirm docker docker-compose git
+fi
 
-systemctl enable --now docker.service
+# If Debian-based
+if [[ -e /etc/debian_version ]]
+then
+        apt-get update 
+        apt-get install -y docker.io docker-compose git
+fi
 
-useradd -m -s /bin/bash vulnerable \
-    && echo 'vulnerable:vulnerable' | chpasswd \
-    && usermod -aG docker vulnerable \
-    && echo 'vulnerable ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/vulnerable \
-    && echo 'source ~/.bash_profile' > /home/vulnerable/.bashrc \
-    && echo 'docker exec -it vulnerable bash' > /home/vulnerable/.bash_profile
+# Start docker service
+systemctl enable docker.service
+systemctl start docker.service
 
-echo 'root:vulnerable' | chpasswd
-
-docker ps -a | grep -q vulnerable || \
-    docker run -d --restart=unless-stopped --name=vulnerable \
-    -p 21:21 \
-    -p 22:22 \
-    -p 80:80 \
-    -p 445:445 \
-    -p 631:631 \
-    -p 3000:3000 \
-    -p 3500:3500 \
-    -p 6697:6697 \
-    -p 3306:3306 \
-    -p 8181:8181 \
-    heywoodlh/sc-vulnerable
+# Clone the repository and bring up the containers
+rm -rf /opt/SC2022-red-team-community
+git clone https://github.com/heywoodlh/SC2022-red-team-community /opt/SC2022-red-team-community
+cd /opt/SC2022-red-team-community/docker && docker-compose up -d 
