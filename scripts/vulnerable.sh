90091306618106d4b0a169e0315fc45783bcd5e3
Spencer Heywood <l.spencer.heywood@protonmail.com>
2022-10-04 22:21:55 -0600

    updated vulnerable.sh and docker-compose.yml-system
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- scripts/vulnerable.sh
+++ scripts/vulnerable.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# If Arch Linux
+# If Arch Linux, install docker
 if grep -q 'Arch Linux' /etc/os-release
 then
         pacman -Syu --noconfirm docker docker-compose git
 fi
 
-# If Debian-based
+# If Debian-based, install docker
 if [[ -e /etc/debian_version ]]
 then
         apt-get update 
@@ -17,8 +17,19 @@ fi
 systemctl enable docker.service
 systemctl start docker.service
 
-# Clone the repository and bring up the containers
-rm -rf /opt/SC2022-red-team-community
-git clone https://github.com/heywoodlh/SC2022-red-team-community /opt/SC2022-red-team-community
-cp -f /opt/SC2022-red-team-community/docker/docker-compose.yml-system /opt/SC2022-red-team-community/docker/docker-compose.yml
-cd /opt/SC2022-red-team-community/docker && docker-compose up -d 
+# If connected to the internet
+if ping -c 1 github.com &>/dev/null
+then
+	# Bring down the existing containers if they exist
+	[[ -d /opt/SC2022-red-team-community/docker ]] && cd /opt/SC2022-red-team-community/docker && docker-compose down
+	cd 
+
+	# Re-clone the repository so it's fresh/up-to-date
+	git clone https://github.com/heywoodlh/SC2022-red-team-community /opt/SC2022-red-team-community.new 
+	[[ -d /opt/SC2022-red-team-community.new ]] && rm -rf /opt/SC2022-red-team-community 
+	[[ -d /opt/SC2022-red-team-community.new ]] && mv /opt/SC2022-red-team-community.new /opt/SC2022-red-team-community
+
+	# Bring the containers back up
+	cp -f /opt/SC2022-red-team-community/docker/docker-compose.yml-system /opt/SC2022-red-team-community/docker/docker-compose.yml
+	cd /opt/SC2022-red-team-community/docker && docker-compose up -d 
+fi
